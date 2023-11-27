76afc83cbe19d5606cac32e653177d86861678ba
superfilar <mateusz9650@gmail.com>
2023-11-27 19:09:32 +0100

    hw09 v1.0

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+stopButton.disabled = true;
+let timer = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+
+  timer = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(timer);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+});
---
+++ src/js/02-timer.js
@@ -0,0 +1,74 @@
+import flatpickr from '/node_modules/flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const datePicker = document.querySelector('#datetime-picker');
+const timerModule = document.querySelector('.timer');
+const startButton = document.querySelector('button[data-start]');
+const seconds = document.querySelector('span[data-seconds]');
+const minutes = document.querySelector('span[data-minutes]');
+const hours = document.querySelector('span[data-hours]');
+const days = document.querySelector('span[data-days]');
+
+startButton.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      Notiflix.Notify.failure('Wybierz datę z przyszłości');
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  },
+};
+
+flatpickr(datePicker, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+startButton.addEventListener('click', () => {
+  let timer = setInterval(() => {
+    let countdown = new Date(datePicker.value) - new Date();
+    startButton.disabled = true;
+    if (countdown >= 0) {
+      let timeObject = convertMs(countdown);
+      days.textContent = addLeadingZero(timeObject.days);
+      hours.textContent = addLeadingZero(timeObject.hours);
+      minutes.textContent = addLeadingZero(timeObject.minutes);
+      seconds.textContent = addLeadingZero(timeObject.seconds);
+      if (countdown <= 10000) {
+        timerModule.style.color = 'tomato';
+      }
+    } else {
+      Notiflix.Notify.success('Odliczanie zakończono');
+      timerModule.style.color = 'black';
+      clearInterval(timer);
+    }
+  }, 1000);
+});
